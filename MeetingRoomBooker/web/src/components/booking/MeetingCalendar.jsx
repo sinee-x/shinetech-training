fdc474f6c4788cfd2775b13b43e53a3aef40dbd8
Peyton <zhoupeng@shinetechsoftware.com>
2025-02-22 00:45:50 +0800

    update calendar

--- MeetingRoomBooker/web/src/components/booking/MeetingCalendar.jsx
+++ MeetingRoomBooker/web/src/components/booking/MeetingCalendar.jsx
@@ -1,7 +1,3 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { getBookingsByUserId } from '../../services/bookingService';
-import { useAuth } from '../../auth/AuthContext';
-import { } from '@mui/material'
 import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
 import {
     createViewDay,
@@ -9,42 +5,21 @@ import {
     createViewMonthGrid,
     createViewWeek,
 } from '@schedule-x/calendar'
-import { createEventsServicePlugin } from '@schedule-x/events-service'
 import '@schedule-x/theme-default/dist/index.css'
-const MeetingCalendar = () => {
-    const [events, setEvents] = useState([]);
-    const { user } = useAuth();
-    const fetchBookings = useCallback(() => {
-        if (user?.id) {
-            getBookingsByUserId(user.id).then(data => {
-                const events = data?.map((element) => ({
-                    id: element.id,
-                    title: element.subject + '-' + element.roomName + '-' + element.userName,
-                    start: element.startTime,
-                    end: element.endTime
-                }));
-                setEvents(events);
-            });
-        }
-    }, [user?.id]);
+import React from 'react'
 
-    useEffect(() => {
-        fetchBookings();
-    }, [fetchBookings]);
-
-    const eventsService = useState(() => createEventsServicePlugin())[0]
-
+function MeetingCalendar({ events }) {
+    console.log("events", events)
     const calendar = useCalendarApp({
         views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
         events: events,
-        plugins: [eventsService],
-        defaultView: 'monthGrid',
+        defaultView: 'month-grid',
     })
-
     return (
-        <>
+        <div>
             <ScheduleXCalendar calendarApp={calendar} />
-        </>
-    );
+        </div>
+    )
 }
+
 export default MeetingCalendar;
--- MeetingRoomBooker/web/src/pages/Booking.jsx
+++ MeetingRoomBooker/web/src/pages/Booking.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import MeetingCalendar from "../components/booking/MeetingCalendar";
-
-
+import { useEffect, useState, useCallback } from 'react'
+import { getBookingsByUserId } from '../services/bookingService';
+import { useAuth } from '../auth/AuthContext';
+import dayjs from 'dayjs';
 const Booking = () => {
+  const [events, setEvents] = useState([]);
+  const { user } = useAuth();
+  const fetchBookings = useCallback(() => {
+    if (user?.id) {
+      getBookingsByUserId(user.id).then(data => {
+        const events = data?.map((element) => ({
+          id: element.id,
+          title: element.subject,
+          people: element.userName,
+          location: element.roomName,
+          start: dayjs(element.startTime).format('YYYY-MM-DD HH:mm'),
+          end: dayjs(element.endTime).format('YYYY-MM-DD HH:mm')
+        }));
+        setEvents(events);
+      });
+    }
+  }, [user?.id]);
+
+  useEffect(() => {
+    fetchBookings();
+  }, [fetchBookings]);
+
   return (
     <>
-      <MeetingCalendar></MeetingCalendar>
+      {events.length > 0 && <MeetingCalendar events={events} />}
     </>
   );
 }
